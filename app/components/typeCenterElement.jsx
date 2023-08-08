51f00fa1caffb7c3fa38661f349c3c11a523d106
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-08-08 12:46:38 +0900

    테스트를 위한 console.log 추가

--- app/components/typeCenterElement.jsx
+++ app/components/typeCenterElement.jsx
@@ -9,6 +9,7 @@ export default function TypeCenterElement({ 센터 }) {
     센터아이디_변경(센터.id);
     센터상세페이지열기();
     지역선택(센터.region_slug);
+    console.log('in TypeCenterElement.jsx', 센터)
   };
 
   return <li className={`px-3 py-2 -mx-3 rounded cursor-pointer hover:bg-hoverbg hover:text-primary ${센터아이디 === 센터.id ? 'bg-hoverbg text-primary' :''}`} onClick={()=>{센터상세페이지상태변경()}}>{센터.title}</li>;
