0e2315a771e1c01520bcb7495f889873f0faeefc
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-08-08 22:33:27 +0900

    cache 옵션 추가

--- app/lib/getAllTypeName.jsx
+++ app/lib/getAllTypeName.jsx
@@ -1,12 +1,9 @@
 export default async function GetAllTypeName() {
-    const 응답 = await fetch(
-      "https://cnmapping.cafe24.com/wp-json/custom/v1/center-type-terms"
-    );
-  
-    if (!응답.ok) {
-      throw new Error(`데이터를 불러오지 못했습니다.`);
-    }
-  
-    return 응답.json();
+  const 응답 = await fetch("https://cnmapping.cafe24.com/wp-json/custom/v1/center-type-terms", { cache: "no-cache" });
+
+  if (!응답.ok) {
+    throw new Error(`데이터를 불러오지 못했습니다.`);
   }
-  
+
+  return 응답.json();
+}
