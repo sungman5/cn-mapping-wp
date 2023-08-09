669aa96806338e00444780da4e89183f03d60c76
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-08-09 22:33:39 +0900

    태양 및 통계 추가

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Nav from "./components/nav";
 import CenterDetail from "./components/centerDetail";
 import Header from "./components/header";
 import CnMap from "./components/mapping";
+import CnSun from "./components/sun";
 
 export const metadata = {
   title: "디디다: 충청남도 청소년 시설 자원 맵",
@@ -14,7 +15,6 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-
   const 프로그램목록 = await GetAllPrograms();
 
   return (
@@ -26,7 +26,36 @@ export default async function RootLayout({ children }) {
           <main className="p-6 bg-white border-r w-80">{children}</main>
           <CenterDetail />
           <Programs 프로그램목록={프로그램목록} />
-          <div className="flex items-center justify-center flex-1 h-[calc(100vh-6rem)] p-8 bg-gradient-to-t to-[#C9FFFC] from-[#8BC0D8]">
+          <div className="flex items-center justify-center flex-1 h-[calc(100vh-6rem)] p-8 bg-gradient-to-t to-[#C9FFFC] from-[#8BC0D8] relative">
+            <div className="absolute flex gap-2 bottom-8 left-8">
+              <div className="flex gap-4 px-6 py-4 bg-white rounded-full shadow-sm ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
+                <h1 className="text-xl font-bold font-Pretendard text-primary">충청남도 청소년 인구</h1>
+                <p className="text-xl">
+                  2,126,374명 <span className="text-sm text-applegray">&#40;2023년 6월 기준&#41;</span>
+                </p>
+              </div>
+              <Link href={'/all-center-list'} className="flex gap-4 px-6 py-4 bg-white rounded-full shadow-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+                <h1 className="text-xl font-bold font-Pretendard text-primary">청소년 시설 수</h1>
+                <p className="text-xl">
+                  44개소
+                </p>
+              </Link>
+            </div>
+            <CnSun />
             <CnMap />
           </div>
         </section>
--- app/region/layout.js
+++ app/region/layout.js
@@ -1,11 +1,15 @@
 import RegionNameBtn from "@/app/components/regionName";
 import GetAllRegionName from "@/app/lib/getAllRegionName";
+import Image from "next/image";
 
 export default async function RegionLayout(props) {
   const 지역이름목록 = await GetAllRegionName();
   return (
     <main>
-      <h1 className="mb-2 text-lg font-bold font-Pretendard">지역별</h1>
+      <h1 className="flex items-center gap-1 mb-2 text-lg font-bold font-Pretendard">
+        <span>지역별 </span>
+        {/* <Image className="-translate-y-0.5" src={"cn-nav-region-2.svg"} alt="지역아이콘" width={24} height={24} /> */}
+      </h1>
       <ul className="grid grid-cols-5 gap-1 mb-8 text-sm">
         {지역이름목록.map((지역명) => {
           return <RegionNameBtn key={지역명.id} 지역명={지역명} />;
