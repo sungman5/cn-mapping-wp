c5add2b1e184175804085c59c4b7af39be0799d5
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-08-14 00:52:46 +0900

    api 주소 업데이트

--- app/lib/getAllRegionName.jsx
+++ app/lib/getAllRegionName.jsx
@@ -1,5 +1,5 @@
 export default async function GetAllRegionName() {
-  const 응답 = await fetch("http://cn-mapping-wp.local/wp-json/custom/v1/center-region-terms", { cache: "no-cache" });
+  const 응답 = await fetch("https://cnmapping.cafe24.com/wp-json/custom/v1/center-region-terms", { cache: "no-cache" });
   if (!응답.ok) {
     throw new Error(`데이터를 불러오지 못했습니다.`);
   }
--- app/lib/getSpecificProgramData.jsx
+++ app/lib/getSpecificProgramData.jsx
@@ -1,5 +1,5 @@
 export default async function GetSpecificProgramData(id) {
-    const 응답 = await fetch(`http://cn-mapping-wp.local/wp-json/custom/v1/specific_program/${id}`, {cache : 'no-cache'});
+    const 응답 = await fetch(`https://cnmapping.cafe24.com/wp-json/custom/v1/specific_program/${id}`, {cache : 'no-cache'});
     if(!응답.ok){
         throw new Error(`데이터를 불러오지 못했습니다.`);
     }
