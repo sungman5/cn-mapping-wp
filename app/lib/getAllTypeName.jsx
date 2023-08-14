21afe7b6cce5a2f1dbc224dc073432929edaf4aa
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2023-08-15 00:25:41 +0900

    Use Originam WP Rest API

--- app/lib/getAllTypeName.jsx
+++ app/lib/getAllTypeName.jsx
@@ -1,5 +1,5 @@
 export default async function GetAllTypeName() {
-  const 응답 = await fetch("https://cnmapping.cafe24.com/wp-json/custom/v1/center-type-terms", { cache: "no-cache" });
+  const 응답 = await fetch("https://cnmapping.cafe24.com/wp-json/wp/v2/type", { cache: "no-cache" });
 
   if (!응답.ok) {
     throw new Error(`데이터를 불러오지 못했습니다.`);
